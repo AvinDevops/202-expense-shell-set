6b4e29dd4227f1a92307d6081a99298fa92ffb20
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-27 17:24:36 +0530

    disk usage

--- 03-disk-usage.sh
+++ 03-disk-usage.sh
@@ -6,8 +6,8 @@ DISK_THRESHOLD=10
 
 while IFS= read -r line
 do
-    echo $line
-    # FOLDER=$($line | awk -F " " '{print $NF}')
-    # DISK_USAGE=$($line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    # echo "Folder is $FOLDER, Disk usage is $DISK_USAGE, Threshold is $THRESHOLD "
+    #echo $line
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    DISK_USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    echo "Folder is $FOLDER, Disk usage is $DISK_USAGE, Threshold is $THRESHOLD "
 done <<< $DISK_FILE_SYS
