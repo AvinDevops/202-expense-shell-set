bd9224e4880901bdd8499d1fc54bacc42e42e4bd
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-27 17:34:49 +0530

    disk usage

--- 03-disk-usage.sh
+++ 03-disk-usage.sh
@@ -10,9 +10,9 @@ do
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     DISK_USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
-    if [ $DISK_USAGE -gt $DISK_THRESHOLD]
+    if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
     then
         echo "your folder:$FOLDER disk usage:$DISK_USAGE has reached threshold:$DISK_THRESHOLD"
     fi 
-    
+
 done <<< $DISK_FILE_SYS
