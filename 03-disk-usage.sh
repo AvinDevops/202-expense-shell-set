3ba19c3ce3b9fefb700560e07f04ff28acbc1551
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-27 17:41:11 +0530

    disk usage

--- 03-disk-usage.sh
+++ 03-disk-usage.sh
@@ -14,9 +14,9 @@ do
 
     if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
     then
-         MESSAGE+="your folder:$FOLDER disk usage:$DISK_USAGE has reached threshold:$DISK_THRESHOLD"
+         MESSAGE+="your folder:$FOLDER disk usage:$DISK_USAGE has reached threshold:$DISK_THRESHOLD\n"
     fi 
 
 done <<< $DISK_FILE_SYS
 
-echo $MESSAGE
+echo  -e $MESSAGE
